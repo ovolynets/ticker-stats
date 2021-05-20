199039a59d133e104c5fd70b44ec822ea7cba686
Oleksandr Volynets <oleksandr.volynets@gmail.com>
2021-05-20 23:57:36 +0200

    Added javadoc description of interface methods

--- src/main/java/de/ovolynets/tickerstats/service/TickerService.java
+++ src/main/java/de/ovolynets/tickerstats/service/TickerService.java
@@ -6,7 +6,26 @@ import de.ovolynets.tickerstats.controller.TickerStatistics;
 import java.util.Optional;
 
 public interface TickerService {
+    /**
+     * Add tick data into the database
+     *
+     * @param tick tick data containing instrument name, price and timestamp
+     * @return {@code false} if data is older than 60 seconds, {@code true} otherwise
+     */
     boolean addTick(Tick tick);
-    Optional<TickerStatistics> getStatistics();
+
+    /**
+     * Returns overall statistics on all available ticker data
+     *
+     * @return overall statistics
+     */
+    TickerStatistics getStatistics();
+
+    /**
+     * Returns statistics on ticker data for a given instrument ID. If no data is available
+     * for the given instrument, returns {@code Optional.empty()}
+     *
+     * @return statistics data for the instrument
+     */
     Optional<TickerStatistics> getStatistics(String instrumentId);
 }
